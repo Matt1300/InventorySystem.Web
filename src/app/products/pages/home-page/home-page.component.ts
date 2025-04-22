3fb8e89cd7e3a5559ed45929747438289bb28133
Matt1300 <mateogranizo1304@gmail.com>
2025-04-22 12:31:59 -0500

    feat(home): update product and inventory display; enhance batch management with price tracking

--- src/app/products/pages/home-page/home-page.component.ts
+++ src/app/products/pages/home-page/home-page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { NgClass, NgStyle } from '@angular/common';
+import { NgStyle } from '@angular/common';
+import { ProductsService } from '../../services/products.service';
+import { BatchService } from 'src/app/batches/services/batch.service';
+import { GetBatches } from 'src/app/batches/interfaces/Batch';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
@@ -10,17 +14,64 @@ import { NgClass, NgStyle } from '@angular/common';
     MatTableModule,
     MatCardModule,
     MatChipsModule,
-    NgClass,
-    NgStyle
+    NgStyle,
+    CommonModule
   ],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {
-  columnas: string[] = ['fecha', 'producto', 'tipo', 'cantidad'];
+export class HomePageComponent implements OnInit {
 
-  movimientos = [
-    { fecha: '19/04/2025', producto: 'Monitor LED 24"', tipo: 'Entrada', cantidad: 15 },
-    { fecha: '18/04/2025', producto: 'Teclado mecánico', tipo: 'Salida', cantidad: 5 },
-    { fecha: '17/04/2025', producto: 'Mouse inalámbrico', tipo: 'Salida', cantidad: 8 }
-  ];
+  ngOnInit(): void {
+    this.getTotalProducts();
+    this.getLastBatches();
+  }
+
+  productService = inject(ProductsService);
+  batchService = inject(BatchService);
+  totalProducts = signal<number>(0);
+  lastBadges = signal<GetBatches[]>([]);
+  totalPrice = signal<number>(0);
+
+  movimientos: any[] = [];
+  columnas: string[] = ['fecha', 'numeroLote', 'producto', 'cantidad', 'price'];
+
+  getTotalProducts() {
+    this.productService.getProducts().subscribe({
+      next: (res) => {
+        const productsActive = res.data.filter(product => product.isActive === true);
+        this.totalProducts.set(productsActive.length);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  getLastBatches() {
+    this.batchService.getBatches().subscribe({
+      next: (res) => {
+        this.totalPrice.set(res.data.reduce((acc, batch) => acc + batch.price, 0));
+        this.lastBadges.set(res.data.slice(0, 3));
+        this.addBatchesToMovements(this.lastBadges());
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  addBatchesToMovements(batches: GetBatches[]) {
+    this.movimientos = [];
+    batches.forEach(batch => {
+      const movimiento = {
+        fecha: batch.entryDate,
+        numeroLote: batch.batchNumber,
+        producto: batch.nameProduct,
+        cantidad: batch.quantity,
+        price: batch.price,
+      };
+
+      this.movimientos.push(movimiento);
+    });
+  }
 }
